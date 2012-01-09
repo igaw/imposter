1effb0e3c684137500a32fdc5d442835993c1632
Daniel Wagner <daniel.wagner@bmw-carit.de>
2012-01-09 13:00:44 +0100

    Fix copyright statement

--- imposter.py
+++ imposter.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2012  Daniel Wagner <wagi@monom.org>. All rights reserved.
+#  Copyright (C) 2012  BMW Car IT GmbH. All rights reserved.
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License version 2 as
